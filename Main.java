f87a50760f9a8393a5c144895b6cc7ed9ecef7d2
Szeckir <80286620+Szeckir@users.noreply.github.com>
2023-11-12 01:36:43 -0300

    adc: conquistaTerra, perdeTerra, arrumado no guereiro morreu e outros testes

--- Main.java
+++ Main.java
@@ -6,11 +6,13 @@ public class Main {
         String name;
         int qtdTerras;
         ArrayList<Node> children;
+        boolean alive;
 
         public Node(String name, int qtdTerras, ArrayList<Main.Node> children) {
             this.name = name;
             this.qtdTerras = qtdTerras;
             this.children = new ArrayList<>();
+            this.alive = true;
         }
 
         public void addChild(Node node) { // Adicionar um filho ao Guerreiro
@@ -32,19 +34,34 @@ public class Main {
                     "], [qtdTerras= " + qtdTerras + "], [children: " + childrens + "]";
         }
 
-        public void guerreiroMorreu(Node node) {
-            if (node.children != null) {
-                int terras = node.qtdTerras;
-                int divisaoTerras = terras / node.children.size();
+        public void guerreiroMorreu() {
+            alive = false;
+            if (children != null) {
+                int terras = qtdTerras;
+                int divisaoTerras = terras / children.size();
 
-                for (int i = 0; i < node.children.size(); i++) {
-                    node.children.get(i).qtdTerras += divisaoTerras;
+                for (int i = 0; i < children.size(); i++) {
+                    if (children.get(i).alive == false) {
+                        System.out.println("Guerreiro " + children.get(i).name + " já está morto");
+                    } else {
+                        children.get(i).qtdTerras += divisaoTerras;
+                    }
                 }
+                qtdTerras = 0;
             } else {
                 System.out.println("Guerreiro não possui filhos");
+                qtdTerras = 0;
             }
         }
-    }
+
+        public void conquistaTerra(int terras) {
+            qtdTerras += terras;
+        }
+
+        public void perdeTerra(int terras) {
+            qtdTerras -= terras;
+        }
+    } 
 
 
     static class Tree {
@@ -74,15 +91,17 @@ public class Main {
 
        Node guerreiro4 = new Node("Lucas", 100, null);
        
-
        guerreiro1.addChild(guerreiro2);
        guerreiro1.addChild(guerreiro3);
        
        guerreiro2.addChild(guerreiro4);
 
+        guerreiro2.conquistaTerra(150);
+
         System.out.println(tree.isEmpty());
 
-        guerreiro1.guerreiroMorreu(guerreiro1);
+        guerreiro2.guerreiroMorreu();
+        guerreiro1.guerreiroMorreu();
       
 
         for(int i = 0; i < guerreiro1.children.size(); i++) {
